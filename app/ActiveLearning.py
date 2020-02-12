9794ee5ad798f5debd47546defa15a833487aae0
henryliu00 <henryliuw@gmail.com>
2020-02-13 05:36:25 +0800

    add 'add_mode' and fix bugs in active learning

--- app/ActiveLearning.py
+++ app/ActiveLearning.py
@@ -5,9 +5,10 @@ from sklearn.metrics import explained_variance_score
 import sklearn.gaussian_process as gp
 from sklearn.cluster import SpectralClustering
 import os
+import time
 class ActiveLearner:
     ''' for active learning, basically do selection for users '''
-    def __init__(self, train_X, train_Y, test_X, test_Y, initial_size, add_size, kernel_config, learning_mode, train_SMILES, search_size=50):
+    def __init__(self, train_X, train_Y, test_X, test_Y, initial_size, add_size, kernel_config, learning_mode, add_mode, train_SMILES, search_size=50):
         ''' df must have the 'graph' column '''
         self.train_X = train_X.reset_index().drop(columns='index')
         self.train_Y = train_Y.reset_index().drop(columns='index')
@@ -18,6 +19,7 @@ class ActiveLearner:
         self.search_size = search_size
         self.kernel_config = kernel_config
         self.learning_mode = learning_mode
+        self.add_mode = add_mode
         self.logger = open('active_learning.log', 'w')
         self.plotout = pd.DataFrame({'size': [], 'mse': [], 'r2': [], 'ex-var': [], 'alpha': []})
         self.train_SMILES = train_SMILES.reset_index().drop(columns='index')
@@ -34,8 +36,9 @@ class ActiveLearner:
 
     def train(self, alpha=0.5):
         # continue needs to be added soon
+        self.logger.write(time.asctime( time.localtime(time.time())))
         self.logger.write('Start Training, training size = %i:\n' % len(self.train_smiles))
-        self.logger.write('training smiles: %s\n' % ' '.join(self.train_smiles))
+        # self.logger.write('training smiles: %s\n' % ' '.join(self.train_smiles))
         train_x = self.train_X[self.train_SMILES.SMILES.isin(self.train_smiles)]
         if not self.kernel_config.T:
             train_x = train_x['graph']
@@ -51,9 +54,7 @@ class ActiveLearner:
         self.alpha = alpha
         self.logger.write('training complete, alpha=%3g\n' % alpha)
 
-    def add_samples(self, add_size=None, search_size=None):
-        if add_size is None:
-            add_size = self.add_size
+    def add_samples(self):
         untrain_x = self.train_X[~self.train_SMILES.SMILES.isin(self.train_smiles)]
         if not self.kernel_config.T:
             untrain_x = untrain_x['graph']
@@ -62,30 +63,24 @@ class ActiveLearner:
         if self.learning_mode == 'supervised':
             y_pred = self.model.predict(untrain_x)
             try:
-                untrain_smiles['mse'] = abs(y_pred - np.array(untrain_y))
+                untrain_smiles.loc[:, 'mse'] = abs(y_pred - np.array(untrain_y))
                 group = untrain_smiles.groupby('SMILES')
                 smiles_mse = pd.DataFrame({'SMILES': [], 'mse': []})
                 for i, x in enumerate(group):
-                    smiles_mse.loc[i] = x[0], x[1].mse.max()
+                    smiles_mse.loc[i] = x[0], x[1].mse.max(), self.train_X[self.train_SMILES.SMILES == x[0]]['graph'].tolist()[0]
             except:
                 raise ValueError('Missing value for supervised training')
-            index = smiles_mse['mse'].nlargest(add_size).index
-            self.train_smiles = np.r_[self.train_smiles, smiles_mse[smiles_mse.index.isin(index)].SMILES]
+            add_idx = self._get_samples_idx(smiles_mse, 'mse')
+            self.train_smiles = np.r_[self.train_smiles, smiles_mse[smiles_mse.index.isin(add_idx)].SMILES]
         elif self.learning_mode == 'unsupervised':
             y_pred, y_std = self.model.predict(untrain_x, return_std=True)
-            untrain_smiles['std'] = y_std
+            untrain_smiles.loc[:,'std'] = y_std
             group = untrain_smiles.groupby('SMILES')
             smiles_std = pd.DataFrame({'SMILES': [], 'std': [],'graph':[] })
             for i, x in enumerate(group):
                 smiles_std.loc[i] = x[0], x[1]['std'].max(), self.train_X[self.train_SMILES.SMILES == x[0]]['graph'].tolist()[0]
             #index = smiles_std['std'].nlargest(add_size).index
-            if search_size is None:
-                    search_size = self.search_size
-            if len(smiles_std) < search_size:
-                search_size = len(smiles_std)
-            search_idx = sorted(smiles_std['std'].nlargest(search_size).index)
-            search_graphs = smiles_std[smiles_std.index.isin(search_idx)]['graph']
-            add_idx = self._find_add_idx(search_graphs, add_size)
+            add_idx = self._get_samples_idx(smiles_std, 'std')
             self.train_smiles = np.r_[self.train_smiles, smiles_std[smiles_std.index.isin(add_idx)].SMILES]
         elif self.learning_mode == 'random':
             group = untrain_smiles.groupby('SMILES')
@@ -98,24 +93,47 @@ class ActiveLearner:
         self.current_size = len(self.train_smiles)
         self.logger.write('samples added to training set, currently %d samples\n' % self.current_size)
 
-    def _find_add_idx(self, X, add_sample_size):
+    def _get_samples_idx(self, df, target):
+        ''' get a sample idx list from the pooling set using add mode method 
+        :df: dataframe constructed
+        :target: should be one of mse/std
+        :return: list of idx
+        '''
+        if self.add_mode=='random':
+            return np.random.randint(0, len(df), self.add_size)
+        elif self.add_mode == 'cluster':
+            if self.search_size==0 or len(df) < self.search_size: # from all remaining samples 
+                search_size = len(df)
+            else:
+                search_size = self.search_size
+            search_idx = sorted(df[target].nlargest(search_size).index)
+            search_graphs_list = df[df.index.isin(search_idx)]['graph']
+            add_idx = self._find_add_idx_cluster(search_graphs_list)
+            return np.array(search_idx)[add_idx]
+        elif self.add_mode == 'nlargest':
+            return df[target].nlargest(self.add_size).index
+        else:
+            raise ValueError("unrecognized method. Could only be one of ('random','cluster','nlargest').")
+
+    def _find_add_idx_cluster(self, X):
         ''' find representative samples from a pool using clustering method
         :X: a list of graphs
         :add_sample_size: add sample size
+        :return: list of idx
         '''
         # train SpectralClustering on X
-        if len(X) < add_sample_size:
+        if len(X) < self.add_size:
             return [ i for i in range( len(X))]
         gram_matrix = self.kernel_config.kernel(X)
-        result = SpectralClustering(n_clusters=add_sample_size, affinity='precomputed').fit_predict(gram_matrix) # cluster result
+        result = SpectralClustering(n_clusters=self.add_size, affinity='precomputed').fit_predict(gram_matrix) # cluster result
         # choose the one with least in cluster distance sum in each cluster
         for i in range(len(X)): # edit self distance to be zero
             gram_matrix[i][i] = np.inf
-        total_distance = {i:{} for i in range(add_sample_size)} # (key: cluster_idx, val: dict of (key:sum of distance, val:idx))
+        total_distance = {i:{} for i in range(self.add_size)} # (key: cluster_idx, val: dict of (key:sum of distance, val:idx))
         for i in range(len(X)): # get all in-class distance sum of each item
             cluster_class = result[i]
             total_distance[cluster_class][np.sum((np.array(result) == cluster_class) * 1/gram_matrix[i])] = i
-        add_idx = [total_distance[i][min(total_distance[i].keys())] for i in range(add_sample_size)] # find min-in-cluster-distance associated idx
+        add_idx = [total_distance[i][min(total_distance[i].keys())] for i in range(self.add_size)] # find min-in-cluster-distance associated idx
         return add_idx
 
     def evaluate(self):
@@ -133,4 +151,4 @@ class ActiveLearner:
         if not os.path.exists(os.path.join(os.getcwd(),'result' )):
                 os.makedirs(os.path.join(os.getcwd(), 'result'))
         self.plotout.reset_index().drop(columns='index').\
-            to_csv('result/%s-%s-%d-%d.log' % (self.kernel_config.property, self.learning_mode, self.search_size, self.add_size), sep=' ', index=False)
+            to_csv('result/%s-%s-%s-%d-%d.log' % (self.kernel_config.property, self.learning_mode, self.add_mode, self.search_size, self.add_size), sep=' ', index=False)
