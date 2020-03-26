f539d220bfb618bf03db859d22deb460a760f1ec
XiangyanSJTU <hnxxxy123@sjtu.edu.cn>
2020-03-26 00:08:28 -0400

    small update.

--- scripts/kernelplot.py
+++ scripts/kernelplot.py
@@ -9,15 +9,15 @@ def main():
     parser = argparse.ArgumentParser(description='This is a code to visualize the kernel matrix')
     parser.add_argument('-i', '--input', type=str, help='Input data.')
     parser.add_argument('-p', '--property', type=str, help='Property.')
-    opt = parser.parse_args()
+    args = parser.parse_args()
 
-    kernel_config = KernelConfig(save_mem=False, property=opt.property)
-    X, Y, remove_smiles = get_XY_from_file(opt.input, kernel_config)
+    kernel_config = KernelConfig(save_mem=False, property=args.property)
+    X, Y, remove_smiles = get_XY_from_file(args.input, kernel_config)
     kernel = kernel_config.kernel
     K = kernel(X)
     # plt.imshow(K, cmap=plt.cm.gray)
     plt.hist(K[np.triu_indices(K.shape[0], k=1)], bins=20)
-    plt.savefig('kernelplot.png')
+    plt.savefig('%s.png' % args.property)
     plt.show()
 
 
