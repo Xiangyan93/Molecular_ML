9e6e88cee041ad33b8c9963ec4922ace40828f6e
XiangyanSJTU <hnxxxy123@sjtu.edu.cn>
2020-04-24 02:18:56 +0800

    update

--- scripts/uncertainty_violin_plot.py
+++ scripts/uncertainty_violin_plot.py
@@ -5,18 +5,21 @@ import matplotlib.pyplot as plt
 def main():
     import argparse
     parser = argparse.ArgumentParser(description='tSNE analysis')
-    parser.add_argument('--log', type=str, help='log file.')
-    parser.add_argument('--train', type=str, help='log file of training set.')
+    parser.add_argument('--log', dest='list', nargs='+', type=str, help='log file list.')
+    # parser.add_argument('--train', type=str, help='log file of training set.')
     parser.add_argument('--output', type=str, help='output file.')
-    parser.add_argument('--trainplot', action='store_true', help='plot training set data.')
+    # parser.add_argument('--trainplot', action='store_true', help='plot training set data.')
     args = parser.parse_args()
-
-    df = pd.read_csv(args.log, sep='\s+')
-    df_train = pd.read_csv(args.train, sep='\s+')
-    if args.trainplot:
-        df_untrain = df_train
-    else:
-        df_untrain = df[~df.smiles.isin(df_train.smiles)]
+    
+    df_list = []
+    for log in args.log:
+        df_list.append(pd.read_csv(log, sep='\s+'))
+    df = pd.concate(df_list).reset_index().drop(columns='index')
+    # df_train = pd.read_csv(args.train, sep='\s+')
+    # if args.trainplot:
+        # df_untrain = df_train
+    # else:
+        # df_untrain = df[~df.smiles.isin(df_train.smiles)]
     N = 20
     du = 1 / N
     all_data = []
@@ -24,7 +27,7 @@ def main():
     for i in range(N):
         b = i * du
         e = (i + 1) * du
-        data = df_untrain[(df_untrain.uncertainty > b) & (df_untrain.uncertainty < e)]
+        data = df[(df.uncertainty > b) & (df.uncertainty < e)]
         if len(data) > 0:
             print(b, e, len(data))
             pos.append((e-b)/2+b)
