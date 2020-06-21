896110acfa7c04a5b40f19598df8cdd13ff5dea4
XiangyanSJTU <hnxxxy123@sjtu.edu.cn>
2020-06-21 15:46:13 -0400

    update.

---
+++ codes/hashgraph.py
@@ -0,0 +1,37 @@
+from rdkit.Chem import AllChem as Chem
+from graphdot import Graph
+
+
+class HashGraph(Graph):
+    def __init__(self, smiles=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.smiles = smiles
+
+    def __eq__(self, other):
+        if self.smiles == other.smiles:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if self.smiles < other.smiles:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        if self.smiles > other.smiles:
+            return True
+        else:
+            return False
+
+    def __hash__(self):
+        return hash(self.smiles)
+
+    @classmethod
+    def from_inchi(cls, inchi):
+        mol = Chem.MolFromInchi(inchi)
+        g = cls.from_rdkit(mol)
+        g.smiles = Chem.MolToSmiles(mol)
+        return g
+
