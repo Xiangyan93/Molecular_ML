39f144d6971bcf5ed60d4f72ef2befdfd7fc7a0d
XiangyanSJTU <hnxxxy123@sjtu.edu.cn>
2020-07-20 10:30:29 -0400

    update.

--- codes/gpr.py
+++ codes/gpr.py
@@ -408,8 +408,6 @@ class RobustFitGaussianProcessRegressor(GPR):
 
     def predict(self, *args, **kwargs):
         result = super().predict(*args, **kwargs)
-        print(1)
-
         if self.y_scale:
             if type(result) is tuple:
                 y_back = self.scaler.inverse_transform(result[0].reshape(-1, 1)).flatten()
